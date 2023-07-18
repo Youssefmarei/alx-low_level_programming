36d1fbf519f4fb16bf3b7ce1b92037fd7b66fce1
Youssef Ahmed <yusufaam@hotmail.com>
2023-07-18 14:56:54 -0700

    main file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
+
+int putchar(char c);
+void printAlphabet(void);
+void printAlphabet_x10(void);
+int islower(int c);
+int isalpha(int c);
+int printSign(int n);
+int abs(int);
+int printLast_digit(int);
+void jackBauer(void);
+void timesTable(void);
+int add(int, int);
+void printTo98(int n);
+void printTimesTable(int n);
+
+#endif /* MAIN_H */
